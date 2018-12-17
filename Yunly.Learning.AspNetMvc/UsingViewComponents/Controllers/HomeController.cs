fa3e2020c9f6d3b5f61e3d50712e21df4c8be77a
yunly@live.Zack <yunly@live.cn>
2018-12-17 16:50:52 -0400

    1217

---
+++ Yunly.Learning.AspNetMvc/UsingViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        public HomeController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Product newProduct)
+        {
+            repository.AddProduct(newProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
